e8c34e6c9e1478f4e69fcdf7356612f1f27fe8cf
Christian Schweiger <3153713+ChSchweiger@users.noreply.github.com>
2021-04-23 22:52:41 +0200

    Added missing ExecutionMode

--- Calculator/src/at/tuwien/calc/command/operations/logical/NegationOperation.java
+++ Calculator/src/at/tuwien/calc/command/operations/logical/NegationOperation.java
@@ -1,12 +1,14 @@
 package at.tuwien.calc.command.operations.logical;
 
 import at.tuwien.calc.command.ICommand;
+import at.tuwien.calc.command.annotation.ExecutionMode;
 import at.tuwien.calc.context.IContext;
 import at.tuwien.calc.model.DoubleDataEntry;
 import at.tuwien.calc.model.ListDataEntry;
 
 import java.util.regex.Pattern;
 
+@ExecutionMode
 public class NegationOperation implements ICommand {
 
     private static final Pattern pattern = Pattern.compile("~");
