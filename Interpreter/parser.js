fd7492e6ab3055455b5aa3f7dc30afeb3e64a872
Christian Schweiger <3153713+ChSchweiger@users.noreply.github.com>
2021-05-28 16:35:05 +0200

    Added structure for parser

--- Interpreter/parser.js
+++ Interpreter/parser.js
@@ -1,5 +1,19 @@
 class Parser {
-    
+  expr() {
+    // TODO: Not Implemented yet
+  }
+
+  apply() {
+    // TODO: Not Implemented yet
+  }
+
+  basic() {
+    // TODO: Not Implemented yet
+  }
+
+  pairs() {
+    // TODO: Not Implemented yet
+  }
 }
 
 export default Parser;
