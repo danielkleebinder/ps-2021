f00ca5ab3faf439cfed6038b2b722808c18752db
Christian Schweiger <3153713+ChSchweiger@users.noreply.github.com>
2021-06-21 11:43:48 +0200

    Added demo mode and argument handling

--- Interpreter/src/main.js
+++ Interpreter/src/main.js
@@ -1,18 +1,16 @@
 import Interpreter from "./interpreter/interpreter.js";
 import Lexer from "./lexer/lexer.js";
 import Parser from "./parser/parser.js";
-
+import parseArgs from "minimist";
 
 /**
- * Run the process function to evaluate some program code.
+ * Run the processInput function to evaluate some program code.
  * @param input Program code.
  * @param debugDetails If true, additional debug details will be print out.
  * @returns {*}
  */
-const process = (input, debugDetails = true) => {
-  if (debugDetails) {
-    console.log("Evaluating:", input);
-  }
+const processInput = (input, debugDetails = false) => {
+  console.log("Evaluating:", input);
   let lexer = new Lexer();
   let tokenized = lexer.tokenize(input);
   if (debugDetails) {
@@ -29,35 +27,51 @@ const process = (input, debugDetails = true) => {
   let result = interpreter.interpret(ast);
   if (debugDetails) {
     console.log("Result:", result);
+  } else {
+    if (result instanceof Array) {
+      console.log("Result:", result[result.length -1])
+    } else {
+      console.log("Result:", result);
+    }
   }
 
   return result;
 };
 
-process("plus 1 2");
-process("minus 2 -3");
-process("plus(mult 2 2)3");
-process("cond 1 8");
-process("cond (plus 0 1) 999");
-process("{a=1}");
-process("{a=1,b=10}");
-process("{a=1,b=mult(plus 3 2)5}");
-process("{x=1}x");
-process("{a = x -> 1} a 1");
-process("{a = x -> x} a 3");
-process("{a = x -> mult(plus 1 x) 3} a 3");
-process("{ a = x -> x } a 3");
-process("{}");
-process("{ a = x -> x, b = x -> mult(plus 1 x) a(5) } b 2");
-process("{ a = x -> x, b = x -> cond x 2 3 } b(0)");
-process("{ a = x -> x, b = x -> { res = a(x) } } b(999) res");
-process("{ d=x->mult x x, v=d 2 } v");
-process("{ a=x->y->mult x y, b=a 2, c=b 3 } c");
-process("{ a=x->y->plus(mult x x)y, b=a 2, c=b 3 } c");
-process("{a=x->y->plus(mult x x)y, b=a 2, c=b 3}minus(b 5)c");
-process("{a=x->{head=a head}}a");
-process("{ a = x -> mult x x, b = y -> a y } b 10");
+const args = parseArgs(process.argv);
+const debug = args["v"] || args['verbose'];
 
-// process("{append = x->y->cond x {head=x head, tail=append(x tail)y} y, gen = x->cond x (append(gen(minus x 1)) {head=x, tail={}}) {}} gen 3");
+if (args["d"] || args["demo"]) {
+  processInput("plus 1 2", debug);
+  processInput("minus 2 -3", debug);
+  processInput("plus(mult 2 2)3", debug);
+  processInput("cond 1 8", debug);
+  processInput("cond (plus 0 1) 999", debug);
+  processInput("{a=1}", debug);
+  processInput("{a=1,b=10}", debug);
+  processInput("{a=1,b=mult(plus 3 2)5}", debug);
+  processInput("{x=1}x", debug);
+  processInput("{a = x -> 1} a 1", debug);
+  processInput("{a = x -> x} a 3", debug);
+  processInput("{a = x -> mult(plus 1 x) 3} a 3", debug);
+  processInput("{ a = x -> x } a 3", debug);
+  processInput("{}", debug);
+  processInput("{ a = x -> x, b = x -> mult(plus 1 x) a(5) } b 2", debug);
+  processInput("{ a = x -> x, b = x -> cond x 2 3 } b(0)", debug);
+  processInput("{ a = x -> x, b = x -> { res = a(x) } } b(999) res", debug);
+  processInput("{ d=x->mult x x, v=d 2 } v", debug);
+  processInput("{ a=x->y->mult x y, b=a 2, c=b 3 } c", debug);
+  processInput("{ a=x->y->plus(mult x x)y, b=a 2, c=b 3 } c", debug);
+  processInput("{a=x->y->plus(mult x x)y, b=a 2, c=b 3}minus(b 5)c", debug);
+  processInput("{a=x->{head=a head}}a", debug);
+  processInput("{ a = x -> mult x x, b = y -> a y } b 10", debug);
+} else {
+  if (!args['_'][2]) {
+    console.log("Missing expression");
+    console.log("Usage: interpreter [--demo] | \"<expression>\"");
+    process.exit(128)
+  }
+  processInput(args['_'][2], debug)
+}
 
-export default process;
+export default processInput;
